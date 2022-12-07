7c5f61e5341763723d411841c0d023c61c3fa8fe
ChunRen Zhang <rayatn1011@gmail.com>
2022-12-07 23:21:36 +0800

    chore: The web page will be opened automatically after the server is started

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import eslint from 'vite-plugin-eslint'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), eslint()],
+    server: {
+        open: true,
+    },
 })
