cc41ad030cc7512a25e2a685db432a089e3783f9
ChunRen Zhang <rayatn1011@gmail.com>
2022-12-07 23:25:54 +0800

    docs: more rigorous type check, which is optional

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,10 @@ module.exports = {
         'plugin:react-hooks/recommended',
         'plugin:jsx-a11y/recommended',
         'plugin:@typescript-eslint/recommended',
+        /**
+         * This would be a more rigorous type check, which I think is optional
+         */
+        // 'plugin:@typescript-eslint/recommended-requiring-type-checking',
         'plugin:prettier/recommended',
     ],
     overrides: [],
