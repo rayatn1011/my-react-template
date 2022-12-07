2ca18363f020a23a019500b2440de3d28f520454
ChunRen Zhang <rayatn1011@gmail.com>
2022-12-07 23:20:10 +0800

    chore: make prettier rules better

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,5 +2,14 @@ module.exports = {
     trailingComma: "es5",
     tabWidth: 4,
     semi: false,
-    singleQuote: true
+    singleQuote: true,
+    printWidth: 100,
+    overrides: [
+        {
+          files: ['*.html'],
+          options: {
+            tabWidth: 2,
+          },
+        },
+      ],
 }
