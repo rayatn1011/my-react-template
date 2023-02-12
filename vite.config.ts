1f5eddc4c57bee1bf2be290ccf8a1d46193db6ac
ChunRen Zhang <rayatn1011@gmail.com>
2023-02-13 00:01:20 +0800

    feat: remove useless plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [react()],
   server: {
     open: true,
   },
